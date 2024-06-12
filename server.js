682086792abb9040eba79339a3711b573393b66d
bryanandradesz <bryan.andrade@aluno.ifsp.edu.br>
2024-06-12 09:07:01 -0300

    cart system

--- server.js
+++ server.js
@@ -11,47 +11,59 @@ now = new Date
 // ------------------ Express e EJS --------------------- //
 
 app.use(express.static(__dirname + '/public'))
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname+"/public/scripts/"));
+app.set('views', path.join(__dirname + "/public/scripts/"));
 
 
 // ------------------ Variaveis Globais --------------------- //
 
-const Carrinho = require('./src/model/Carrinho.js') 
+const Carrinho = require('./src/model/Carrinho.js')
+var itemsCarrinho = 0
 let login = false
 const vetor = new Array()
+
 
 
 // ------------------Principais--------------------- //
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname+'/index.html')
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.get("/menu", function (req, res) {
-    res.sendFile(__dirname+'/public/scripts/menu.html')
+    res.sendFile(__dirname + '/public/scripts/menu.html')
 })
 
+
+
 app.get("/header", function (req, res) {
-    
-    if(login) {
-        
-        res.render("header", {login_section:"Minha ", login_section2:"Conta", num_itemscart:0})
+
+    if (login) {
+
+        res.render("header", { 
+             login_section: "Minha ",
+             login_section2: "Conta", 
+             numCarrinho: itemsCarrinho 
+            })
     } else {
-        res.render("header", {login_section:"Olá! Acesse sua conta ", login_section2:"Login!", num_itemscart:0})
+        res.render("header", { 
+            login_section: "Olá! Acesse sua conta ", 
+            login_section2: "Login!", 
+            numCarrinho: itemsCarrinho 
+        })
     }
 })
 
 
 app.get('/body', function (req, res) {
-    res.sendFile(__dirname+'/public/scripts/body.html')
+    res.sendFile(__dirname + '/public/scripts/body.html')
 })
 // -----------------Rotas Navegação---------------------- //
 
 app.get('/account', function (req, res) {
     if (login) {
-         res.render("account")
+        res.render("account")
     } else {
         res.render("login")
     }
@@ -62,32 +74,27 @@ app.get('/cardapio', function (req, res) {
 
 // ----------------- Carrinho ---------------------- //
 app.post('/addcarrinho', function (req, res) {
-    const botao = String(req.body.b2)
-    console.log(botao)
 
-        
-})
-
-// -----------------------------------------------------------
-
-
-app.post('/addcarrinho', function (req, res) {
-    //o bloco try catch trata exceções
     try {
-        const botao = String(req.body.b2)
         let obj = new Carrinho()
-        let ingredientes = String(req.body.inIngredientes)
-        let sabor = String(req.body.inSabor)
-        let = valor = parseFloat(req.body.inValor)
-
-        console.log(ingredientes, sabor, valor)
-
+        obj.sabor = String(req.body.inSabor);
+        obj.ingredientes = String(req.body.inIngredientes);
+        obj.valor = parseFloat(req.body.inValor);
+        itemsCarrinho += 1
+        console.log(`--------< Novo Pedido! >--------\nPizza: ${obj.sabor}\nIngredientes: ${obj.ingredientes}\nValor: ${obj.valor}`)
+        res.redirect("/mostrarCarrinho")
+        vetor.push(obj)
     }
     catch (erro) {
         console.log("Erro: " + erro) // vai para o terminal
         res.send("Erro: " + erro) // vai para o browser
     }
 })
+
+// -----------------------------------------------------------
+
+
+
 
 app.get('/listar', function (req, res) {
     try {
@@ -99,8 +106,9 @@ app.get('/listar', function (req, res) {
     }
 })
 
-app.get("/remover/:pos", function (req, res) {
 
+app.get("/remover/:pos", function (req, res) {
+    itemsCarrinho -= 1
     let pos = parseInt(req.params.pos)
     vetor.splice(pos, 1) // remove na posição e a quantidade
     res.redirect('/listar')
@@ -108,22 +116,20 @@ app.get("/remover/:pos", function (req, res) {
 
 
 
-
-
 // ------------------Iniciar Servidor--------------------- //
 
 app.use((req, res, next) => {
     res.status(404).send("Desculpe, não conseguimos encontrar essa página.");
-  }) // Erro 404
+}) // Erro 404
 
 
-  app.listen(port, function(erro) {
-    if(erro){
+app.listen(port, function (erro) {
+    if (erro) {
         console.log("❌ » Erro :" + erro)
     }
-    else{
+    else {
         console.clear()
-        console.log("✅ » Servidor Online atualizado ás " + now.getHours() + ":" + now.getMinutes() + " na porta 3000...") 
+        console.log("✅ » Servidor Online atualizado ás " + now.getHours() + ":" + now.getMinutes() + " na porta 3000...")
     }
 })
 
